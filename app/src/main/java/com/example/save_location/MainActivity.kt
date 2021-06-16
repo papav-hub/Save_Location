b86523177629cbe22625b78371f12cc56918ebaf
papav-hub <pavi03@kpu.ac.kr>
2021-06-17 04:40:58 +0900

    이상한 위치정보 가져오기 완료

--- app/src/main/java/com/example/save_location/MainActivity.kt
+++ app/src/main/java/com/example/save_location/MainActivity.kt
@@ -4,10 +4,7 @@ import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
-import android.location.Location
-import android.location.LocationManager
+import android.location.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +13,9 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,61 +31,62 @@ class MainActivity : AppCompatActivity() {
     var fname : String  = ""
     var str : String = ""
 
-    var locationManager : LocationManager? = null
-    private val REQUEST_CODE_LOCATION : Int = 2
-    var currentLocation : String = ""
-    var latitude : Double? = null
-    var longitude : Double? = null
-
-    private fun getLatLng() : Location {
-        var currentLatLng : Location? = null
-        if(ActivityCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), this.REQUEST_CODE_LOCATION)
-
-            getLatLng()
-        }else{
-            val locationProvider = LocationManager.GPS_PROVIDER
-            currentLatLng = locationManager?.getLastKnownLocation(locationProvider)
-        }
-        return currentLatLng!!
-    }
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 
-    private fun getCurrentLoc(){
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
-        var userLocation : Location = getLatLng()
-        if(userLocation != null){
-            latitude = userLocation.latitude
-            Log.d("CheckCurrentLocation", "현재 내 위치 값 : $latitude, $longitude")
-            var mGeocoder = Geocoder(applicationContext, Locale.KOREAN)
-            var mResultList : List <Address>? = null
-            try{
-                mResultList = mGeocoder.getFromLocation(
-                    latitude!!, longitude!!, 1
-                )
-            }catch(e: IOException){
-                e.printStackTrace()
-            }
-            if(mResultList != null){
-                Log.d("CheckCurrentLocation", mResultList[0].toString())
-                /*Log.d("CheckCurrentLocation", mResultList[0].toString(), getAddressLine(0))*/
-                currentLocation = mResultList[0].getAddressLine(0)
-                /*currentLocation = currentLocation.substring(11)*/
-            }
-        }
-    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var L : String = ""
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    L = location.toString()
+                }
+
+
+        L_Btn.setOnClickListener {// 위치 불러오기 버튼
+            L_textView.setText(L)
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
         @SuppressLint("WrongConstant")
         fun saveDiary(readyDay: String) {/////////////////////////////////////////////////////저장
             var fos: FileOutputStream? = null
 
             try {
-                fos = openFileOutput(readyDay, MODE_NO_LOCALIZED_COLLATORS)
+                fos = openFileOutput(readyDay, Context.MODE_PRIVATE)
+
                 var content: String = editTextTextMultiLine.getText().toString()
                 fos.write(content.toByteArray())
                 fos.close()
@@ -96,18 +97,12 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        L_Btn.setOnClickListener {
-            getCurrentLoc()
-            val L : String = "위도:" + latitude.toString() + " 경도:" + longitude.toString() + " 좌표:" + currentLocation
-            L_textView.setText(L)
-        }
-
         @SuppressLint("WrongConstant")
         fun removeDiary(readyDay: String) {/////////////////////////////////////////////////////삭제
             var fas: FileOutputStream? = null
 
             try {
-                fas = openFileOutput(readyDay, MODE_NO_LOCALIZED_COLLATORS)
+                fas = openFileOutput(readyDay, Context.MODE_PRIVATE)
                 var content: String = ""
                 fas.write(content.toByteArray())
                 fas.close()
@@ -152,20 +147,14 @@ class MainActivity : AppCompatActivity() {
             false
         }
 
-        calendarView.setOnDateChangeListener{view, year, month, dayOfMonth ->
-            /*diaryTextView.visibility = View.VISIBLE
-            editTextTextMultiLine.visibility = View.VISIBLE
-            save_Btn.visibility = View.VISIBLE
-            textView2.visibility = View.VISIBLE
-            cha_Btn.visibility = View.VISIBLE
-            del_Btn.visibility = View.VISIBLE*/
+        calendarView.setOnDateChangeListener{view, year, month, dayOfMonth -> // 날자 바꾸기
 
             diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
             textView2.setText("")
             checkedDay(year, month, dayOfMonth)
         }
 
-        save_Btn.setOnClickListener{
+        save_Btn.setOnClickListener{// save 버튼
             saveDiary(fname)
             var t1 = Toast.makeText(this, fname + "데이터를 저장했습니다.", Toast.LENGTH_SHORT)
             t1.show()
